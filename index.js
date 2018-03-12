df6b039511b03a1bce67c2165da040c8c67e9e41
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-12 18:55:33 +0700

    index.js

--- index.js
+++ index.js
@@ -18,14 +18,22 @@ app.get('/remove/:id', (req, res) => {
 });
 
 app.post('/add', parser, (req, res) => {
+    const { name, link, image } = req.body;
+    const singer = new Singer({ name, link, image });
+    singer.save()
+    .then(() => res.redirect('/'))
+    .catch(error => res.send(error));
 });
-
 app.post('/update/:id', parser, (req, res) => {
 });
 
 app.get('/add', (req, res) => res.render('add'));
 
 app.get('/update/:id', (req, res) => {
+    const { id } = req.params;
+    Singer.findById(id)
+    .then(singer => res.render('update', { singer }))
+    .catch(error => res.send(error));
 });
 
 app.listen(3000, () => console.log('Server started!'));
