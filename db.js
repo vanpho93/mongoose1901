4e1544fd0704a44986d95b66f64adf5c2fd88e4c
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-12 18:20:39 +0700

    insertMany

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 mongoose.connect('mongodb://localhost/mean1901');
 
 const singerSchema = new mongoose.Schema({
@@ -7,9 +6,9 @@ const singerSchema = new mongoose.Schema({
     image: { type: String, trim: true, required: true },
     link: { type: String, trim: true, required: true, unique: true }
 });
-
 const Singer = mongoose.model('Singer', singerSchema);
 
+/* create new doc
 const singer = new Singer({ 
     name: 'Duc Phuc', 
     image: '1.png', 
@@ -20,4 +19,20 @@ singer.save()
 .then(s => console.log(s))
 .catch(error => console.log(error));
 
-// console.log(singer);
+Singer.insertMany(
+    [
+        { 
+            name: 'Erik', 
+            image: '2.png', 
+            link: 'https://fb.com/erik' 
+        },
+        { 
+            name: 'Hoa Minzy', 
+            image: '3.png', 
+            link: 'https://fb.com/hoa-minzy' 
+        }
+    ]
+)
+.then(s => console.log(s))
+.catch(error => console.log(error));
+*/
