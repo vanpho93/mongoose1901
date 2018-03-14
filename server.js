90a0963a84b4213475ddb96e36be4788606b4c81
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-14 18:12:50 +0700

    public

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const multer = require('multer');
+
+const upload = multer({ dest: 'public/' });
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 app.get('/', (req, res) => res.render('upload'));
 
-app.post('/upload', (req, res) => {
+app.post('/upload', upload.single('singerImage'), (req, res) => {
     res.send('OK');
 });
 
