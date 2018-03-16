ac575cdb50d461841f89ae85df135872b5c0a7c4
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-16 18:44:44 +0700

    middleware

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const reload = require('reload');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', (req, res) => res.send('Hello.'));
+app.use(express.static('public'));
+app.use((req, res, next) => {
+    console.log(req.path);
+    next();
+});
+
+// next.js
+
+// const myMiddleware = (req, res, next) => {
+//     res.send('123');
+//     // req.name = 'abcd';
+//     // next();
+// };
+
+app.get('/', (req, res, next) => {
+    // console.log('Name =', req.name);
+    // res.render('app');
+    // res.send('a');
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    // console.log('Name =', req.name);
+    // res.render('app');
+    res.send('b');
+});
+
+// app.get('*', (req, res) => res.send('404'));
+
 app.listen(3000, () => console.log('Server started!'));
+reload(app);
+
+// middleware
